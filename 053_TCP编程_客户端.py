bb50e29e506f0f0c0ed02bca2e349f306937988a
Mr1207 <1436005686@qq.com>
2020-02-15 20:56:45 +0800

    053

--- 053_TCP编程_客户端.py
+++ 053_TCP编程_客户端.py
@@ -1,30 +1,14 @@
 # 我们要创建一个基于TCP连接的Socket
 import socket
 
-# 创建一个socket:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# 连接服务，指定主机和端口
-s.connect(('127.0.0.1', 3000))
-
-# 发送数据：
-s.send(b'hello')
-
-# 接收数据:
-# 接收数据时，调用recv(max)方法，一次最多接收指定的字节数，
-# 因此，在一个while循环中反复接收，直到recv()返回空数据，表示接收完毕，退出循环。
-buffer = []
-while True:
-    # 每次最多接收1k字节:
-    d = s.recv(1024)
-    if d:
-        buffer.append(d)
-    else:
-        break
-# join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串。
-data = b''.join(buffer)
-
-# 关闭连接:
+# 建立连接:
+s.connect(('127.0.0.1', 9999))
+# 接收欢迎消息:
+print(s.recv(1024).decode('utf-8'))
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
 s.close()
-
-print(data.code('utf-8'))
--- 053_TCP编程_服务端.py
+++ 053_TCP编程_服务端.py
@@ -1,5 +1,7 @@
-# 我们要创建一个基于TCP连接的Socket
+# 我们要创建一个基于TCP连接的Socket(翻译：插座)
 import socket
+import threading
+import time
 
 # 服务器进程首先要绑定一个端口并监听来自其他客户端的连接。如果某个客户端连接过来了，
 # 服务器就与该客户端建立Socket连接，随后的通信就靠这个Socket连接了。
@@ -18,11 +20,26 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 也可以用0.0.0.0绑定到所有的网络地址，还可以用127.0.0.1绑定到本机地址。127.0.0.1是一个特殊的IP地址，
 # 表示本机地址，如果绑定到这个地址，客户端必须同时在本机运行才能连接，也就是说，外部的计算机无法连接进来。
 # 监听端口:
-s.bind(('127.0.0.1', 3000))
+s.bind(('127.0.0.1', 9999))
 
 # 紧接着，调用listen()方法开始监听端口，传入的参数指定等待连接的最大数量：
 s.listen(5)
 print('Waiting for connection...')
+
+
+# 子线程的程序
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
+
 
 # 接下来，服务器程序通过一个永久循环来接受来自客户端的连接，accept()会等待并返回一个客户端的连接
 while True:
@@ -30,6 +47,6 @@ while True:
     # accept()
     # 当一个客户端连接时，它将返回一个新的 socket 对象,你将用这个 socket 对象和客户端进行通信
     sock, addr = s.accept()
-
-
-# 未完待续。。。。
+    # 每个连接都必须创建新线程（或进程）来处理，否则，单线程在处理连接的过程中，无法接受其他客户端的连接
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
